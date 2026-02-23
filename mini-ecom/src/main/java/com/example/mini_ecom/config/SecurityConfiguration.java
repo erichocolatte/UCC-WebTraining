1dfada695575ecf4b00e129b3e99b8fef2a475a2
vanduc006 <bagiadaohoa@gmail.com>
2026-02-23 17:07:18 +0700

    fix cors

--- mini-ecom/src/main/java/com/example/mini_ecom/config/SecurityConfiguration.java
+++ mini-ecom/src/main/java/com/example/mini_ecom/config/SecurityConfiguration.java
@@ -125,22 +125,24 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:3000",
-            "http://localhost:5500",
-            "http://127.0.0.1:5500",
-            "http://52.77.223.254:80",
-            "http://52.77.223.254"
+
+        configuration.setAllowedOriginPatterns(List.of(
+            "http://52.77.223.254",
+            "http://localhost:*",
+            "http://127.0.0.1:*"
         ));
-        configuration.setAllowedOriginPatterns(List.of("*"));
+
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
+
     
 }
